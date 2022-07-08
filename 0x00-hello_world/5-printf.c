2382a44c01b94aa6b7033e37342b4ace4059212e
TracyM93 <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@github.com>
2022-07-08 01:13:24 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
- * Description: main - Printsout a string to stdout.
- * Return: 0 if success
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
